fa99b04fafa6d64c2ff6c436cf00a16dbe01819c
Speedking <michaelmartens578@gmail.com>
2025-03-07 22:45:02 +0100

    NEW: Skills

--- js/information.js
+++ js/information.js
@@ -1,17 +1,17 @@
 document.getElementById("info-text").addEventListener("click", function () {
     // Info-Box anzeigen
     document.getElementById("info-box").style.display = "block";
-    document.getElementById("overlay").classList.add("active");
+    document.getElementById("info-overlay").classList.add("active");
 
-    // Alle anderen Inhalte ausblenden
-    document.querySelector(".container").style.display = "none";
+    // Verstecke den Rest der Seite
+    document.body.classList.add("no-scroll");
 });
 
 document.getElementById("close-info").addEventListener("click", function () {
     // Info-Box ausblenden
     document.getElementById("info-box").style.display = "none";
-    document.getElementById("overlay").classList.remove("active");
+    document.getElementById("info-overlay").classList.remove("active");
 
-    // Alle anderen Inhalte wieder anzeigen
-    document.querySelector(".container").style.display = "block";
+    // Seite wieder normal anzeigen
+    document.body.classList.remove("no-scroll");
 });
